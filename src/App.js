ec71a8ff0735d4fd41fd6044f4b0fbb6bad030f4
Robert King <robert.o.king@hotmail.co.uk>
2021-03-09 22:23:31 +0100

    Added: QUESTIONS, state.currentQuestionID used to fill prompt

--- src/App.js
+++ src/App.js
@@ -6,16 +6,40 @@ import './App.css';
 import Prompt from './components/Prompt.jsx';
 import AnswerInput from './components/AnswerInput.jsx';
 
+const QUESTIONS = [
+  { prompt: '한글',
+    answers: ['hangeul'],
+    translation: 'Hangeul (or Hangul following old romanisation) - the Korean alphabet' }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const newQuestionID = Math.floor(Math.random() * QUESTIONS.length);
+
+    this.state = { currentQuestionID: newQuestionID };
+  }
+
+  setNewQuestion = () => {
+    let newQuestionID = Math.floor(Math.random() * QUESTIONS.length);
+    this.setState({ currentQuestionID: newQuestionID });
+  }
+
   checkAnswer = (answer) => {
-    console.log(answer);
+    const currentQuestion = QUESTIONS[this.state.currentQuestionID];
+    if (currentQuestion.answers.includes(answer.toLowerCase())) {
+      console.log('Correct!');
+    } else {
+      console.log('Wrong!');
+    }
   };
 
   render() {
     return (
       <div className="App">
         <h1>Up to Speed: Reading Korean</h1>
-        <Prompt prompt='한' />
+        <Prompt prompt={QUESTIONS[this.state.currentQuestionID].prompt} />
         <AnswerInput checkAnswer={this.checkAnswer} />
       </div>
     );
